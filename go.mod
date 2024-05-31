e3e3529bef3ccc229b0830f407f31ba0bbe46c24
Daniel R <nzdanron@gmail.com>
2024-06-01 11:16:39 +1200

    feat: connect to db
    - Connected to go and fetched events

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/danielronalds/go-restful-api
 
 go 1.22.2
+
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/lib/pq v1.10.9
+)
--- src/main.go
+++ src/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Event struct {
+	Id          int       `db:"id"`
+	Name        string    `db:"name"`
+	Description string    `db:"description"`
+	Startdate   string `db:"startdate"`
+	Enddate     string `db:"enddate"`
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	db, err := sqlx.Connect("postgres", "user=admin dbname=mydb sslmode=disable password=admin host=postgres")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	// Test the connection to the database
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Successfully Connected")
+	}
+
+	event := Event{}
+
+	rows, _ := db.Queryx("SELECT * FROM api.Events")
+
+	for rows.Next() {
+		err := rows.StructScan(&event)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("%#v\n", event)
+	}
 }
